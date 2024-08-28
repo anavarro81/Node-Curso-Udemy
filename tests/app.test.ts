576de93d0b2d309686960e6873af5f16678fa160
Antonio Navarro <anavarro67@hotmail.com>
2024-08-28 10:30:20 +0200

    feat: Ejercicios TS y JEST
    Ejercicios TS y JEST

---
+++ tests/app.test.ts
@@ -0,0 +1,13 @@
+describe('App', () => {
+    test('should be true', () => {
+
+
+        const num1=20
+        const num2=30
+
+        const result = num1 + num2
+
+
+        expect(result).toBe(20)
+    })
+})
